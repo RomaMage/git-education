1b18bffb3eb2591d4890dc0b8cf12f575f909064
Babaev Roman <roman.babaev@altexsoft.com>
2019-07-29 18:35:35 +0300

    Work code but not optimized

--- app.js
+++ app.js
@@ -3,70 +3,118 @@
 const form = document.querySelector('.task-form');
 const taskField = document.querySelector('#task_name');
 const taskListWrapper = document.querySelector('.task-list-wrapper');
-const ul = document.createElement('ul');
-const input = document.createElement('input');
-let li;
-let list;
+let checkList = checkTasksList();
 
-//create input
-function createInput(input, type, text) {
-    if (!type) return;
-    input.setAttribute('type', type);
-    if ((type === 'submit' || type === 'button') && text)  {
-        input.setAttribute('value', text)
-    }
-    return input;
+if (checkList[1]) {
+    initHtmlList();
 }
-
-//add eventlistener
 form.addEventListener('submit', addTask);
 
-//add task to list
+//check if input value not empty and add task
 function addTask(e) {
     e.preventDefault();
-
-    let taskText;
+    let taskText = '';
+    let list = JSON.parse(localStorage.getItem('taskList'));
 
     if (taskField.value === '') {
-        alert('please add Task');
+        alert('Please Add Task');
     } else {
+        index = (list !== null) ? list.length : 0;
         taskText = taskField.value;
-        addToStorage(taskText);
-        renderTaskList();
+        addToTasks(taskText);
+        addHtmlListItem(index, taskText);
+    }
+}
+
+// Add to Local Storage
+function addToTasks(text) {
+    let list = new Array();
+    let checkList = checkTasksList();
+    if (checkList[1]) {
+        list = JSON.parse(localStorage.getItem('taskList'));
     }
 
-    clearButton = taskListWrapper.querySelector('input');
-    clear.addEventListener('click', clearTask);
+    list.splice(list.length, 0, text);
+    return localStorage.setItem('taskList', JSON.stringify(list));
 }
-
-//add to localStorage 
-function addToStorage(text) {
-    let storageList = localStorage.getItem('taskList');
-    let listArray = new Array();
-    if (storageList == '') {
-        localStorage.setItem('taskList', '');
-    } else {
-        listArray = JSON.parse(localStorage.getItem('taskList'));    
+// Init Html List
+function initHtmlList() {
+    const ul = document.createElement('ul');
+    ul.className = 'task-list';
+    taskListWrapper.appendChild(ul);
+    if (checkList[1]) {
+        let list = JSON.parse(localStorage.getItem('taskList'));
+        list.forEach(function(item, index){
+            addHtmlListItem(index, item);
+        });
     }
-    listArray[listArray.length] = text;
-    return localStorage.setItem('taskList', JSON.stringify(listArray));
+
+    return taskListWrapper.querySelector('ul');
 }
 
-//render task list
-function renderTaskList() {
-    if (localStorage.getItem('taskList') == '') return;
-    listArray = JSON.parse(localStorage.getItem('taskList'));
-    list = taskListWrapper.appendChild(ul);
-    button = createInput(input, 'submit', 'Clear Task');
-    taskListWrapper.appendChild(button);
-    listArray.forEach(function(e){
-        li = document.createElement('li');
-        li.innerText = e;
-        list.appendChild(li);
-    });
+function addHtmlListItem(index, text) {
+    if (text === 'undefined') return;
+    let list;
+    list = (taskListWrapper.querySelector('ul')) ? taskListWrapper.querySelector('ul') : initHtmlList();
+    li = document.createElement('li');
+    li.className = 'row-item';
+    li.setAttribute('data-id', index);
+    li.innerText = text;
+    a = document.createElement('a');
+    a.setAttribute('href', '#');
+    a.innerText = 'x';
+    a.className = 'remove-item';
+    li.appendChild(a);
+    list.appendChild(li);
+    if (!taskListWrapper.querySelector('input[type="button"]')) addClearTasksButton();
+
+    let removeItems = taskListWrapper.querySelectorAll('.remove-item');
+    if (removeItems) {
+        removeItems.forEach(function(element){
+            element.addEventListener('click', removeItem);
+        });
+    }
 }
 
-//clear all tasks
-function clearTask() {
+function addClearTasksButton() {
+    const button = document.createElement('input');
+
+    button.className = 'remove-tasks';
+    button.setAttribute('type', 'button');
+    button.setAttribute('value', 'Clear Tasks');
+    button.addEventListener('click', clearAllTasks);
+
+    return taskListWrapper.appendChild(button);
+}
+
+function removeItem(event) {
+    let clickedLink = event.target;
+    let parentItem = clickedLink.parentNode;
+    parentItem.parentNode.removeChild(parentItem);
+    let list = JSON.parse(localStorage.getItem('taskList'));
+    if (list) {
+        list.splice(parentItem.dataset.id, 1);
+    }
+    localStorage.setItem('taskList', JSON.stringify(list));
+}
+
+function clearAllTasks() {
     localStorage.clear();
+    taskListWrapper.innerHTML = '';
+}
+
+// Check function with error text
+function checkTasksList() {
+    let taskListErrors = new Array();
+    switch (true) {
+        case JSON.parse(localStorage.getItem('taskList') === null):
+            taskListErrors = {0 : "task list empty"};
+            break;
+        case JSON.parse(localStorage.getItem('taskList') !== null):
+            taskListErrors = {1 : "task list full"};
+            break;
+        default: 
+            taskListErrors = {2 : "other errors"};
+    }
+    return taskListErrors;
 }

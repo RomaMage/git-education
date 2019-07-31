f3ac9e4cf9103814ccacac0235c18d9e316caf50
Babaev Roman <roman.babaev@altexsoft.com>
2019-07-31 17:51:06 +0300

    create new task list with classes

--- app.js
+++ app.js
@@ -1,63 +1,32 @@
-//Define variable
-
-const form = document.querySelector('.task-form');
-const taskField = document.querySelector('#task_name');
-const taskListWrapper = document.querySelector('.task-list-wrapper');
-const ul = document.createElement('ul');
-const li = document.createElement('li');
-const input = document.createElement('input');
-
-//add eventlistener
-form.addEventListener('submit', addTask);
-
-//create input
-function createInput(input, type, text) {
-    if (!type) return;
-    input.setAttribute('type', type);
-    if ((type === 'submit' || type === 'button') && text)  {
-        input.setAttribute('value', text)
+class Task {
+    constructor(title, priority, description) {
+        this.title = title;
+        this.priority = priority;
+        this.description = description;
+        
     }
-    return input;
 }
 
-//add task to list
-function addTask(e) {
-    let taskText;
+function UI(){}
 
-    if (taskField.value === '') {
-        alert('please add Task');
-    } else {
-        taskText = taskField.value;
-        createTaskList(taskText);
-    }
+UI.prototype.addTaskToList = function(task){
+    const listWrapper = document.querySelector('.task-list-wrapper table');
+
+    const li = document.createElement('li');
+    
+    li.innerHtml = `${task.title} ${task.priority} $`;
+}
+
+document.querySelector('.task-form').addEventListener('submit', function(e){
+    const title = document.getElementById('task-title');
+    const priority = document.getElementById('task-priority');
+    const description = document.getElementById('task-description');
+
+    const task = new Task(title, priority, description);
+
+    const ui = new UI();
+
+    ui.addTaskToList(task);
 
     e.preventDefault();
-}
-
-//init task list
-function initTaskList(taskListWrapper) {
-    let list = taskListWrapper.querySelector('ul');
-    let clearButton = createInput(button);
-
-    if (!list) {
-        taskListWrapper.appendChild(ul);
-        list = taskListWrapper.querySelector('ul');
-        taskListWrapper.appendChild(button);
-        button.innerText = ''
-    }
-
-    return list;
-}
-
-//create task list
-function createTaskList(taskText) {
-    let list = initTaskList(taskListWrapper);
-    
-    return createTask(list, taskText);
-}
-
-//add item to task list
-function createTask(ul, text) {
-    li.innerText = text;
-    return ul.appendChild(li);
-}
+});

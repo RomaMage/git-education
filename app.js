66c9315005fe5ba4f15ec2440a6409d1d2547f35
Babaev Roman <roman.babaev@altexsoft.com>
2019-08-05 12:37:52 +0300

    create task list by classes

--- app.js
+++ app.js
@@ -3,24 +3,25 @@ class Task {
         this.title = title;
         this.priority = priority;
         this.description = description;
-        
     }
 }
 
 function UI(){}
 
 UI.prototype.addTaskToList = function(task){
-    const listWrapper = document.querySelector('.task-list-wrapper table');
+    const listWrapper = document.querySelector('.task-list-wrapper table tbody');
 
-    const li = document.createElement('li');
+    const row = document.createElement('tr');
     
-    li.innerHtml = `${task.title} ${task.priority} $`;
+    row.innerHTML = `<td>${task.title}</td><td>${task.priority}</td><td>${task.description}</td>`;
+
+    listWrapper.append(row);
 }
 
 document.querySelector('.task-form').addEventListener('submit', function(e){
-    const title = document.getElementById('task-title');
-    const priority = document.getElementById('task-priority');
-    const description = document.getElementById('task-description');
+    const title = document.getElementById('task-title').value;
+    const priority = document.getElementById('task-priority').value;
+    const description = document.getElementById('task-description').value;
 
     const task = new Task(title, priority, description);
 

b17c962c638edffed7eb057b13dca370a64aec69
roman.babaev <roman.babaev@altexsoft.com>
2020-02-14 12:37:56 +0200

    create grid of canvases

--- main.js
+++ main.js
@@ -13,9 +13,7 @@
         
 
         $mapHolder.find('.level-map').each((index, item) => {
-            getLevelValues(item);
-            
-            console.log(getLevelValues(item));
+            createCanvasGrid(canvasSettings, item);
         });
     }
 
@@ -32,17 +30,32 @@
     }
 
     function createCanvasGrid (settings, item) {
-        while (settings.square > grid.square) {
-            createCanvas(item, settings);
+        const div = document.createElement('div');
+        const levelSettings = getLevelValues(item);
+        const canvas = createCanvas(item, canvasSettings);
+        div.className = 'canvas-holder';
+        div.style.position = 'absolute';
+        div.style.top = '0';
+        div.style.left = '0';
+        $(item).append(div);
+
+        let canvasHolderSquare = $(div).innerWidth() * $(div).innerHeight();
+        console.log(levelSettings.square);
+        let canvasCount = Math.round(levelSettings.square / (settings.canvasWidth * settings.canvasHeight));
+        let i=1;
+        while (i < canvasCount) {
+            let canvas = createCanvas(item, settings);
+            $(div).append(canvas);
+            i++;
         }
+        console.log(canvasCount);
     }
 
     function createCanvas(item, settings) {
         const canvas = document.createElement('canvas');
         canvas.style.height = canvasSettings.canvasHeight;
         canvas.style.width = canvasSettings.canvasWidth;
+        return canvas;
     }
-
-
 
 })(jQuery);
